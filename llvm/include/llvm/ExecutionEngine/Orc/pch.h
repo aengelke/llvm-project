36864664345256a853814320bf4ae665381a7ac6
Alexis Engelke <engelke@in.tum.de>
2026-07-28 09:16:12 +0000

    orc pch

---
+++ llvm/include/llvm/ExecutionEngine/Orc/pch.h
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+/// Precompiled header for LLVMOrcJIT.
+///
+//===----------------------------------------------------------------------===//
+
+#include "llvm/ExecutionEngine/Orc/Core.h"
+#include "llvm/IR/pch.h"
+#include "llvm/Support/pch.h"
